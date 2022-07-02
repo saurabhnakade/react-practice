c78e551347c208a3cd1bdf2bb000a94564c1fd0c
saurabhnakade <saurabh.nakade23@gmail.com>
2022-07-02 13:55:00 +0530

    component-lifecycle update Persons.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from './Person.css'  // role of webpack to manage
-// import Radium from "radium";
 
-
-const person = (props) => {
-  console.log("Person.js rendering...");
-  return (
-    <div className={classes.Person} >
-      <p onClick={props.click}>
-        This is {props.name} {props.age} years old
-      </p>
-      <input type="text" onChange={props.change} value={props.name} />
-      <p>{props.children}</p>
-    </div>
-  );
+class Person extends Component{
+  render(){
+    console.log("Person.js rendering...");
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          This is {this.props.name} {this.props.age} years old
+        </p>
+        <input type="text" onChange={this.props.change} value={this.props.name} />
+        <p>{this.props.children}</p>
+      </div>
+    );
+  }
 };
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const Persons = (props) =>{
-  console.log("Persons.js rendering...");
+class Persons extends Component{
+  state = {
+    someValue:'someValue'
+  };
+  static getDerivedStateFromProps(props, state) {
+    console.log("Persons.js get derived state from props", props);
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Persons.js should component update");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("Persons.js get snapshot before update");
+    return { message: "Snapshot" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Persons.js component did update");
+    console.log(snapshot);
+  }
   
-  return props.persons.map((person, index) => {
-    return <Person key={person.id} name={person.name} age={person.age} 
-    click={()=>props.clicked(index)} change={(event)=>props.changed(person.id,event)} />;
-  });
+  render(){
+    console.log("Persons.js rendering...");
+
+    return this.props.persons.map((person, index) => {
+      return <Person key={person.id} name={person.name} age={person.age} 
+      click={() => this.props.clicked(index)} change={(event) => this.props.changed(person.id, event)} />;
+    });
+  }
 }
 
 export default Persons;
