022a64678017537a6366cbe0ed2e8e2a023e5a02
saurabhnakade <saurabh.nakade23@gmail.com>
2022-07-08 15:28:24 +0530

    rendering adjacent JSX ele

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 import classes from './Person.css'  // role of webpack to manage
+import Aux from "../../../hoc/Aux";
 
 class Person extends Component{
   render(){
     console.log("Person.js rendering...");
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
+      // <div className={classes.Person}>
+      <Aux>
+        <p key="i1" onClick={this.props.click}>
           This is {this.props.name} {this.props.age} years old
         </p>
-        <input type="text" onChange={this.props.change} value={this.props.name} />
-        <p>{this.props.children}</p>
-      </div>
+        <input key="i2" type="text" onChange={this.props.change} value={this.props.name} />
+        <p key="i3">{this.props.children}</p>
+      </Aux>
+      // </div>
     );
   }
 };
+
 
 export default Person;
